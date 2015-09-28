a6ae9c7b4ebaf65060f2e9548d2f33f41fc18f35
Witchu Promjunyakul <witchu@debuz.com>
2015-09-28 12:50:23 +0700

    add ci

---
+++ ci-scripts/install_luarocks.bat
@@ -0,0 +1,13 @@
+@rd /s/q c:\work c:\luarocks 2> nul
+set
+pushd c:\
+md \work
+cd \work
+powershell -command "wget 'http://www2.cs.uidaho.edu/~jeffery/win32/unzip.exe' -OutFile 'unzip.exe'"
+powershell -command "wget 'http://keplerproject.github.io/luarocks/releases/luarocks-2.2.2-win32.zip' -OutFile 'luarocks.zip'"
+unzip luarocks.zip
+cd luarocks-2.2.2-win32
+call install /P c:\luarocks /SELFCONTAINED /L /NOADMIN /F /Q
+call c:\luarocks\2.2\luarocks path --bin > setpath.bat
+call setpath.bat
+popd
